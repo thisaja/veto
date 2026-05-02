e3ddca2b38ba07937bc595b2088a463665370e60
Thisaja Alwis <thisajaalwis@gmail.com>
2026-05-02 11:23:17 -0400

    added nativewind (#4)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import "../global.css";
 
 export const unstable_settings = {
   anchor: '(tabs)',
