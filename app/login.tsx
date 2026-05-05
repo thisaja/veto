acbf7c9aef39d06f3ef50bf064fefcfc9e9b6227
Thisaja Alwis <thisajaalwis@gmail.com>
2026-05-04 23:35:27 -0400

    added landing screen

---
+++ app/login.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+const LoginScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>
+                Login Screen
+            </Text>
+        </SafeAreaView>
+    )
+}
+export default LoginScreen
